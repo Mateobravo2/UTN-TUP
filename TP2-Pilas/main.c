ce8a20bcaff81d8458e3e76be2b5e9d5a6899652
Mateobravo2 <88749722+Mateobravo2@users.noreply.github.com>
2026-03-17 12:30:14 -0300

    Update main.c

--- TP2-Pilas/main.c
+++ TP2-Pilas/main.c
@@ -3,58 +3,103 @@
 #include "pila.h"
 
 void menu();
-
-
-
+void cargarValores(int aux, Pila* pilita);
+void pasarValores(Pila *pilita, Pila *pilaux);
+void conservarOrden(Pila *pilita, Pila *pilaux, Pila *pila2);
+void invertirPila(Pila *pilita, Pila *pilaux, Pila *pila2);
 
 int main()
 {
+
     menu();
     return 0;
 }
 
-
 void menu()
 {
-    int op=0;
+    int op;
+    int aux=0;
     Pila pilita;
     inicpila(&pilita);
-
+    Pila pilaux;
+    inicpila(&pilaux);
+    Pila pila2;
+    inicpila(&pila2);
     do
     {
-        printf("Ingrese la operacion que desea realizar [1-6] \n");
-        printf("            _________________________________________\n");
-        printf("            | [1] Cargar valores en pila        |\n");
-        printf("            | [2] Pasar pila                    |\n");
-        printf("            | [3] Pasar pila|\n");
-        printf("            | [4] |\n");
-        printf("            | [5] |\n");
-        printf("            | [6] |\n");
-        printf("            -----------------------------------------\n");
+        printf("________________________________\n");
+        printf("[1] FUNCION DE CARGA            \n");
+        printf("[2] PASAR DE UNA PILA A OTRA    \n");
+        printf("[3] PASAR CONSERVANDO VALORES   \n");
+        printf("[4] INVERTIR PILA               \n");
+        printf("[5]                             \n");
+        printf("[6]                             \n");
+        printf("--------------------------------\n");
         scanf("%i", &op);
+        system("cls");
         switch(op)
         {
-            case 0
+            case 0:
             {
-                printf("~~~ FINALIZANDO EL PROGRAMA ~~~");
+                printf("~ ~ ~ FINALIZANDO PROGRAMA ~ ~ ~");
             }break;
-            case 1
+            case 1:
+            {
+                printf("Cuantos valores desea ingresar en la pila?\n");
+                scanf("%i", &aux);
+                cargarValores(aux, &pilita);
+                system("cls");
+            }break;
+
+            case 2:
+            {
+                pasarValores(&pilita, &pilaux);
+            }break;
+
+            case 3:
+            {
+                conservarOrden(&pilita, &pilaux, &pila2);
+            }break;
+
+            case 4:
+            {
+                invertirPila(&pilita, &pilaux, &pila2);
+            }break;
+
+            case 5:
             {
 
             }break;
-            case 2
-            {
-
-            }break;
-            case 3
         }
-    }while(op!=0)
+    }while(op!=0);
 }
 
-
-void cargarValores()
+void cargarValores(int aux, Pila* pilita)
 {
+    while(aux>0)
+    {
+        leer(pilita);
+        aux--;
+    }
+}
 
+void pasarValores(Pila *pilita, Pila *pilaux)
+{
+    while(pilavacia(pilita)==0)
+    {
+        apilar(pilaux, desapilar(pilita));
+    }
+}
 
+void conservarOrden(Pila *pilita, Pila *pilaux, Pila *pila2)
+{
+    pasarValores(pilita, pilaux);
+    pasarValores(pilaux, pila2);
+}
+
+void invertirPila(Pila *pilita, Pila *pilaux, Pila *pila2)
+{
+    conservarOrden(pilita, pilaux, pila2);
+    pasarValores(pila2, pilita);
 }
 
