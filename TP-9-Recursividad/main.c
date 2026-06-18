ac6bc6d76d49cc9b03fc9df5efa00744f4f80f08
Mateo Bravo <88749722+Mateobravo2@users.noreply.github.com>
2026-06-18 11:10:56 -0300

    Update main.c

--- TP-9-Recursividad/main.c
+++ TP-9-Recursividad/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define DIM 6
 
@@ -18,7 +19,16 @@ int esCapicua(int arr[], int inicio, int fin);
 //PUNTO 6
 int sumarElementosArr(int arr[], int validos, int i, int suma);
 //PUNTO 7
-
+int buscarMenorRecursivo(int arr[], int validos, int menor, int i);
+//PUNTO 8
+void nombreDeArchivo(char nombre[]);
+void cargarArchivo(char nombre[]);
+int buscarMenorEnArchivo(char nombre[]);
+int buscarMenorArchivoRecursivo(FILE *archi, int menor, char nombre[]);
+void mostrarArchivo(char archivo[]);
+//PUNTO 9
+void invertirArchivo(char nombre[]);
+void invertirElementosArchivo(char nombre[], FILE *archi, int j, int i);
 
 int main()
 {
@@ -29,7 +39,8 @@ int main()
 void menu()
 {
     int arr[DIM];
-    int op, num, aux = 0, pot;
+    int op, num, aux = 0, pot, menor;
+    char nombre[15];
     do
     {
         printf("Ingrese el ejercicio que quiere probar: \n");
@@ -39,6 +50,8 @@ void menu()
         printf("| [3] Recorrer arreglo              |\n");
         printf("| [4] Recorrer arreglo invertido    |\n");
         printf("| [5] Descubrir si es capicua       |\n");
+        printf("| [6] Sumar elementos de arreglo    |\n");
+        printf("| [7] Buscar menor en arreglo       |\n");
         printf("| [0] FINALIZAR PROGRAMA            |\n");
         printf("-------------------------------------\n");
         scanf("%i", &op);
@@ -114,6 +127,37 @@ void menu()
                     printf("La suma del arreglo: ");
                     recorrerArregloRecursivo(arr, (DIM-1));
                     printf(" = | %i |", aux);
+                    printf("\n");
+                    system("pause");
+                    system("cls");
+                }break;
+            case 7:
+                {
+                    cargarArregloInt(arr, DIM);
+                    menor = buscarMenorRecursivo(arr, DIM, arr[0], 1);
+                    recorrerArregloRecursivo(arr, DIM-1);
+                    printf("\nEl menor es | %i |\n", menor);
+                    system("pause");
+                    system("cls");
+                }break;
+            case 8:
+                {
+                    nombreDeArchivo(nombre);
+                    cargarArchivo(nombre);
+                    menor = buscarMenorEnArchivo(nombre);
+                    mostrarArchivo(nombre);
+                    printf("\nEl menor es | %i |\n", menor);
+                    system("pause");
+                    system("cls");
+                }break;
+            case 9:
+                {
+                    nombreDeArchivo(nombre);
+                    printf("\nArchivo normal: \n");
+                    mostrarArchivo(nombre);
+                    invertirArchivo(nombre);
+                    printf("\nArchivo invertido: \n");
+                    mostrarArchivo(nombre);
                     printf("\n");
                     system("pause");
                     system("cls");
@@ -231,8 +275,130 @@ int sumarElementosArr(int arr[], int validos, int i, int suma)
 }
 
 //PUNTO 7
-int buscarMenorRecursivo(int arr[], int validos, int i, int menor)
+int buscarMenorRecursivo(int arr[], int validos, int menor, int i)
 {
+    if(i != validos)
+    {
+        if(arr[i] < menor)
+        {
+            menor = arr[i];
+            menor = buscarMenorRecursivo(arr, validos, menor, (i+1));
+        }
+        else
+        {
+            menor = buscarMenorRecursivo(arr, validos, menor, (i+1));
+        }
+    }
+    return menor;
+}
 
-    return i;
+//PUNTO 8
+void nombreDeArchivo(char nombre[])
+{
+    printf("Ingrese el nombre del archivo al que quiere acceder: ");
+    scanf(" %s", nombre);
+    strcat(nombre, ".bin");
+}
+
+void cargarArchivo(char nombre[])
+{
+    int aux;
+    char op = 's';
+    FILE *archi = fopen(nombre, "ab");
+    if(archi != NULL)
+    {
+        while(op != 'n')
+        {
+            printf("Ingrese un numero: ");
+            scanf("%i", &aux);
+            fwrite(&aux, sizeof(int), 1, archi);
+            printf("Quiere seguir ingresando numeros al arreglo? (s/n): ");
+            scanf(" %c", &op);
+        }
+        fclose(archi);
+    }
+}
+
+int buscarMenorEnArchivo(char nombre[])
+{
+    int menor;
+    FILE *archi;
+    archi = fopen(nombre, "rb");
+    if(archi != NULL)
+    {
+        menor = fread(nombre, sizeof(int), 1, archi);
+        menor = buscarMenorArchivoRecursivo(archi, menor, nombre);
+        fclose(archi);
+    }
+    return menor;
+}
+
+int buscarMenorArchivoRecursivo(FILE *archi, int menor, char nombre[])
+{
+    int aux = fread(nombre, sizeof(int), 1, archi);
+    if(!feof(archi))
+    {
+        if(aux < menor)
+        {
+            menor = aux;
+            buscarMenorArchivoRecursivo(archi, menor, nombre);
+        }
+        else
+        {
+            buscarMenorArchivoRecursivo(archi, menor, nombre);
+        }
+    }
+    return menor;
+}
+
+void mostrarArchivo(char archivo[])
+{
+    int aux;
+    FILE *archi;
+    archi = fopen(archivo, "rb");
+    if(archi != NULL)
+    {
+        while(!feof(archi))
+        {
+            fread(&aux, sizeof(int), 1, archi);
+            if(!feof(archi))
+            {
+                printf(" |%i| ", aux);
+            }
+        }
+        fclose(archi);
+    }
+}
+
+//PUNTO 9
+void invertirArchivo(char nombre[])
+{
+    int total;
+    FILE *archi = fopen(nombre, "r+b");
+    if(archi != NULL)
+    {
+        fseek(archi, 0, SEEK_END);
+        total = (ftell(archi) / sizeof(int))-1;
+        printf("\n | %i |\n", total);
+        invertirElementosArchivo(nombre, archi, total, 0);
+        fclose(archi);
+    }
+}
+
+void invertirElementosArchivo(char nombre[], FILE *archi, int j, int i)
+{
+    int num1, num2;
+    if(i < j)
+    {
+        fseek(archi, (sizeof(int)*i), SEEK_SET);
+        fread(&num1, sizeof(int), 1, archi);
+        fseek(archi, sizeof(int)*j, SEEK_SET);
+        fread(&num2, sizeof(int), 1, archi);
+        fseek(archi, sizeof(int)*j, SEEK_SET);
+        fwrite(&num1, sizeof(int), 1, archi);
+        fseek(archi, (sizeof(int)*i), SEEK_SET);
+        fwrite(&num2, sizeof(int), 1, archi);
+        fseek(archi, sizeof(int), SEEK_SET);
+        invertirElementosArchivo(nombre, archi, (j-1), (i+1));
+    }
 }
